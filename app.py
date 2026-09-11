80403780fe96db7d56dd47f8276e4b94562cce31
Tharun Chowdary <ytchowdary2005@gmail.com>
2026-09-11 21:06:47 +0530

    Update FastAPI deployment setup

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-
 from src.rag import ask_question
-
 
 app = FastAPI(title="ShopEase AI Assistant")
 
-
-# Allow the frontend to communicate with the backend
+# Allow frontend-backend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 class Question(BaseModel):
     question: str
 
 
-@app.get("/")
+@app.get("/api")
 def home():
     return {"message": "ShopEase AI Assistant API is running"}
 
 
 @app.post("/ask")
 def ask(data: Question):
-
     answer = ask_question(data.question)
 
     return {
         "answer": answer
     }
+
+
+# Serve frontend
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
